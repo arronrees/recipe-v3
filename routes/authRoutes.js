7e8f6baa4572314ba46ac054e3691f6bb31a04bd
arronrees <64908445+arronrees@users.noreply.github.com>
2022-03-02 18:12:44 +0000

    Added failure flash message for sign in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -27,14 +27,20 @@ passport.use(
 
       // user not found, redirect to signup
       if (!foundUser) {
-        return done(null, false, { message: 'Incorrect credentials' });
+        return done(null, false, {
+          type: 'errorMessage',
+          message: 'Incorrect credentials',
+        });
       }
 
       const passwordMatches = await checkPassword(password, foundUser.password);
 
       // incorrect password
       if (!passwordMatches) {
-        return done(null, false, { message: 'Incorrect credentials' });
+        return done(null, false, {
+          type: 'errorMessage',
+          message: 'Incorrect credentials',
+        });
       }
 
       return done(null, foundUser);
@@ -55,6 +61,7 @@ router.get('/auth/sign-in', getSignInPage);
 router.post(
   '/auth/sign-in',
   passport.authenticate('local', {
+    failureFlash: true,
     failureRedirect: '/auth/sign-in',
   }),
   postSignIn
