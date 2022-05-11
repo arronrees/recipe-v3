ae17d6d2098e804af9dcb419ca8e55293a8b0e4e
arronrees <64908445+arronrees@users.noreply.github.com>
2022-05-11 08:05:30 +0100

    Added search popup

--- public/js/app.js
+++ public/js/app.js
@@ -11,3 +11,26 @@ const navSlide = () => {
 if (!!document.querySelector('.burger')) {
   navSlide();
 }
+
+const showSearch = () => {
+  const btn = document.querySelector('.search');
+  const mobBtn = document.querySelector('.search.mobile__search');
+  const pop = document.querySelector('.search__popup');
+  const close = document.querySelector('.search__popup .close__search');
+
+  btn.addEventListener('click', () => {
+    pop.classList.add('popup--open');
+  });
+
+  mobBtn.addEventListener('click', () => {
+    pop.classList.add('popup--open');
+  });
+
+  close.addEventListener('click', () => {
+    pop.classList.remove('popup--open');
+  });
+};
+
+if (!!document.querySelector('.search')) {
+  showSearch();
+}
